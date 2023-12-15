794c6541bcdaf3acf0379c4174f9ac3090d95bfa
AndriiPolishchuk28 <andrey.work28@gmail.com>
2023-12-15 10:58:14 +0100

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,63 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { nanoid } from 'nanoid';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  addName = (name, number) => {
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    const existContact = this.state.contacts.some(elem => elem.name === name);
+
+    if (!existContact) {
+      this.setState(({ contacts }) => ({
+        contacts: [newContact, ...contacts],
+      }));
+    } else {
+      alert(`${name} is already in contacts`);
+    }
+  };
+
+  handleOnChange = e => {
+    this.setState({ filter: e.target.value });
+  };
+
+  visibleContacts = () => {
+    const { contacts, filter } = this.state;
+    const normalizeFilter = filter.toLowerCase();
+    return contacts.filter(elem =>
+      elem.name.toLowerCase().includes(normalizeFilter)
+    );
+  };
+
+  deleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(elem => elem.id !== id),
+    }));
+  };
+
+  render() {
+    return (
+      <div style={{ marginLeft: '30px' }}>
+        <h1>Phonebook</h1>
+        <ContactForm contacts={this.state.contacts} addHandle={this.addName} />
+        <h2>Contacts</h2>
+        <Filter onChange={this.handleOnChange} />
+        <ContactList
+          onDelete={this.deleteContact}
+          contacts={this.visibleContacts()}
+        />
+      </div>
+    );
+  }
+}
